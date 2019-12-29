44337eccb050581e8bfc93df9620a41ebef046cb
Kolya Opahle <kolyaopahle@gmail.com>
2019-12-30 00:15:43 +0100

    first song list component

---
+++ website/src/app/song-list/song-list.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { SocketService } from '../socket.service';
+
+@Component({
+  selector: 'app-song-list',
+  templateUrl: './song-list.component.html',
+  styleUrls: ['./song-list.component.css']
+})
+export class SongListComponent implements OnInit {
+  songs: string[];
+
+  constructor(private socketService: SocketService) {
+    this.songs = [];
+  }
+
+  ngOnInit() {
+    this.socketService.listSongs$.subscribe(msg => {
+      if (msg.Songs) {
+        this.songs = msg.Songs;
+      }
+    });
+
+    this.socketService.listSongs();
+  }
+
+  refresh() {
+    this.socketService.listSongs();
+  }
+
+  onSongClick(song: string) {
+    this.socketService.addSong(song);
+  }
+}
