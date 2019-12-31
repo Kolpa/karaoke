5f7dae3c80cf1525057fe73b2b065846f66c779d
Kolya Opahle <kolyaopahle@gmail.com>
2019-12-31 15:09:55 +0100

    added static file serving to go

--- cmd/karaoke/main.go
+++ cmd/karaoke/main.go
@@ -4,9 +4,12 @@ import "net/http"
 import "karaoke/internal/server"
 import "karaoke/internal/player"
 
-
 func main() {
-    player.Initialize();
-	http.HandleFunc("/", server.ServeWs)
+	player.Initialize()
+
+	fs := http.FileServer(http.Dir("page"))
+
+	http.Handle("/", fs)
+	http.HandleFunc("/ws", server.ServeWs)
 	http.ListenAndServe("0.0.0.0:8080", nil)
 }
