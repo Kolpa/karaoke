e1bb0b673072672e8041ffb673ba8e25affb9781
Kolya Opahle <kolyaopahle@gmail.com>
2019-12-31 13:51:50 +0100

    new song list component

--- website/src/app/song-list/song-list.component.ts
+++ website/src/app/song-list/song-list.component.ts
@@ -1,31 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { SocketService } from '../socket.service';
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { SocketService, SongInfo } from "../socket.service";
+import { MatTableDataSource } from "@angular/material/table";
+import { MatPaginator } from "@angular/material/paginator";
 
 @Component({
-  selector: 'app-song-list',
-  templateUrl: './song-list.component.html',
-  styleUrls: ['./song-list.component.css']
+  selector: "app-song-list",
+  templateUrl: "./song-list.component.html",
+  styleUrls: ["./song-list.component.css"]
 })
 export class SongListComponent implements OnInit {
-  songs: string[];
+  displayedColumns = ["artist", "title"];
+  songDataSource: MatTableDataSource<SongInfo[]>;
+
+  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
 
   constructor(private socketService: SocketService) {
-    this.songs = [];
+    this.songDataSource = new MatTableDataSource();
   }
 
   ngOnInit() {
+    this.songDataSource.paginator = this.paginator;
+
     this.socketService.listSongs$.subscribe(msg => {
-        this.songs = msg;
+      this.songDataSource.data = msg;
     });
 
     this.socketService.listSongs();
   }
 
-  refresh() {
-    this.socketService.listSongs();
+  applyFilter(filterValue: string) {
+    this.songDataSource.filter = filterValue;
   }
 
-  onSongClick(song: string) {
-    this.socketService.addSong(song);
+  rowSelected(song: SongInfo) {
+    this.socketService.addSong(song.path);
   }
 }
