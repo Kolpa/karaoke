6d8a5f5304ec16fd349613f18f6814eadeece9b3
Kolya Opahle <kolyaopahle@gmail.com>
2019-12-31 13:51:21 +0100

    installed material design

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,22 +1,32 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
-import { SongListComponent } from './song-list/song-list.component';
-import { NowPlayingComponent } from './now-playing/now-playing.component';
-import { QueueComponent } from './queue/queue.component';
+import { IndexPageComponent } from "./index-page/index-page.component";
+import { SongListComponent } from "./song-list/song-list.component";
+import { NowPlayingComponent } from "./now-playing/now-playing.component";
+import { QueueComponent } from "./queue/queue.component";
+import { NoopAnimationsModule } from "@angular/platform-browser/animations";
+import { MatTableModule } from "@angular/material/table";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [
-    AppComponent,
+    IndexPageComponent,
     SongListComponent,
     NowPlayingComponent,
     QueueComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NoopAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [IndexPageComponent]
 })
-export class AppModule { }
+export class AppModule {}
